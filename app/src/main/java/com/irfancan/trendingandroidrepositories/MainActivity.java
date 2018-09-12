f43cd8743bdb5ebf79231f83fff63c40bf2fbc17
IRFAN CAN <irfancan.atay@starcut.com>
2018-09-12 18:15:49 +0300

    Added dependencies required for recyclerview Added RecyclerView to layout Added variables required for RecyclerView

--- app/src/main/java/com/irfancan/trendingandroidrepositories/MainActivity.java
+++ app/src/main/java/com/irfancan/trendingandroidrepositories/MainActivity.java
@@ -2,8 +2,14 @@ package com.irfancan.trendingandroidrepositories;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
